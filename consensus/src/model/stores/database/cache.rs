b9e4cefb44030310a7d11966f420f27027c89e00
msutton <mikisiton2@gmail.com>
2023-01-03 15:20:01 +0200

    Use hashbrown map with unsafe api to remove random elements by index

--- consensus/src/model/stores/database/cache.rs
+++ consensus/src/model/stores/database/cache.rs
@@ -1,18 +1,18 @@
-use indexmap::IndexMap;
-use parking_lot::RwLock;
+use hashbrown::HashMap as BrownHashMap;
+use parking_lot::{lock_api::RwLockWriteGuard, RawRwLock, RwLock};
 use rand::Rng;
 use std::{collections::hash_map::RandomState, hash::BuildHasher, sync::Arc};
 
 #[derive(Clone)]
 pub struct Cache<TKey: Clone + std::hash::Hash + Eq + Send + Sync, TData: Clone + Send + Sync, S = RandomState> {
     // We use IndexMap and not HashMap, because it makes it cheaper to remove a random element when the cache is full.
-    map: Arc<RwLock<IndexMap<TKey, TData, S>>>,
+    map: Arc<RwLock<BrownHashMap<TKey, TData, S>>>,
     size: usize,
 }
 
 impl<TKey: Clone + std::hash::Hash + Eq + Send + Sync, TData: Clone + Send + Sync, S: BuildHasher + Default> Cache<TKey, TData, S> {
     pub fn new(size: u64) -> Self {
-        Self { map: Arc::new(RwLock::new(IndexMap::with_capacity_and_hasher(size as usize, S::default()))), size: size as usize }
+        Self { map: Arc::new(RwLock::new(BrownHashMap::with_capacity_and_hasher(size as usize, S::default()))), size: size as usize }
     }
 
     pub fn get(&self, key: &TKey) -> Option<TData> {
@@ -28,10 +28,20 @@ impl<TKey: Clone + std::hash::Hash + Eq + Send + Sync, TData: Clone + Send + Syn
             return;
         }
         let mut write_guard = self.map.write();
-        if write_guard.len() == self.size {
-            write_guard.swap_remove_index(rand::thread_rng().gen_range(0..self.size));
-        }
+        self.remove_random_if_full(&mut write_guard);
         write_guard.insert(key, data);
+    }
+
+    fn remove_random_if_full(&self, write_guard: &mut RwLockWriteGuard<RawRwLock, BrownHashMap<TKey, TData, S>>) {
+        if write_guard.len() >= self.size {
+            let t = write_guard.raw_table();
+            let i = rand::thread_rng().gen_range(0..t.buckets());
+            unsafe {
+                if t.is_bucket_full(i) {
+                    t.erase(t.bucket(i));
+                }
+            }
+        }
     }
 
     pub fn insert_many(&self, iter: &mut impl Iterator<Item = (TKey, TData)>) {
@@ -40,9 +50,7 @@ impl<TKey: Clone + std::hash::Hash + Eq + Send + Sync, TData: Clone + Send + Syn
         }
         let mut write_guard = self.map.write();
         for (key, data) in iter {
-            if write_guard.len() == self.size {
-                write_guard.swap_remove_index(rand::thread_rng().gen_range(0..self.size));
-            }
+            self.remove_random_if_full(&mut write_guard);
             write_guard.insert(key, data);
         }
     }
@@ -52,7 +60,7 @@ impl<TKey: Clone + std::hash::Hash + Eq + Send + Sync, TData: Clone + Send + Syn
             return;
         }
         let mut write_guard = self.map.write();
-        write_guard.swap_remove(key);
+        write_guard.remove(key);
     }
 
     pub fn remove_many(&self, key_iter: &mut impl Iterator<Item = TKey>) {
@@ -61,7 +69,7 @@ impl<TKey: Clone + std::hash::Hash + Eq + Send + Sync, TData: Clone + Send + Syn
         }
         let mut write_guard = self.map.write();
         for key in key_iter {
-            write_guard.swap_remove(&key);
+            write_guard.remove(&key);
         }
     }
 }
